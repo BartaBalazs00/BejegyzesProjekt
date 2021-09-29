99f3533fd14dee56286cc87cb58a259679972221
Barta Balázs <barta.balazs00@gmail.com>
2021-09-29 15:09:08 +0200

    bejegyzes.txt hozzáadása

--- src/hu/bartabalazs/Main.java
+++ src/hu/bartabalazs/Main.java
@@ -1,8 +1,12 @@
 package hu.bartabalazs;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
     }
 }
