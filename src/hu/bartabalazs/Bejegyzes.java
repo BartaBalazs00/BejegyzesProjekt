c03942765ef1c16fb0a0778bc2634d3b7a40b232
Barta Balázs <barta.balazs00@gmail.com>
2021-09-29 14:45:49 +0200

    változók létrehozása, constructor létrehozása

--- src/hu/bartabalazs/Bejegyzes.java
+++ src/hu/bartabalazs/Bejegyzes.java
@@ -1,5 +1,19 @@
 package hu.bartabalazs;
 
-public class Bejegyzes {
+import java.time.LocalDate;
 
+public class Bejegyzes {
+    private String szerzo;
+    private String tartalom;
+    private int likeok;
+    private LocalDate letrejott;
+    private LocalDate szerkesztve;
+
+    public Bejegyzes(String szerzo, String tartalom) {
+        this.szerzo = szerzo;
+        this.tartalom = tartalom;
+        this.likeok = 0;
+        letrejott = LocalDate.now();
+        szerkesztve = LocalDate.now();
+    }
 }
