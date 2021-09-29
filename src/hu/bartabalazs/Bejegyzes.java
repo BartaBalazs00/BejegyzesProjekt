ea8924369e972a6199c5b7fc108ec4d0e8ab3332
Barta Balázs <barta.balazs00@gmail.com>
2021-09-29 14:48:41 +0200

    getterek setterek létrehozása, metódusok létrehozása

--- src/hu/bartabalazs/Bejegyzes.java
+++ src/hu/bartabalazs/Bejegyzes.java
@@ -16,4 +16,33 @@ public class Bejegyzes {
         letrejott = LocalDate.now();
         szerkesztve = LocalDate.now();
     }
+
+    public String getSzerzo() {
+        return szerzo;
+    }
+
+    public String getTartalom() {
+        return tartalom;
+    }
+
+    public void setTartalom(String tartalom) {
+        this.szerkesztve = LocalDate.now();
+        this.tartalom = tartalom;
+    }
+
+    public int getLikeok() {
+        return likeok;
+    }
+
+    public LocalDate getLetrejott() {
+        return letrejott;
+    }
+
+    public LocalDate getSzerkesztve() {
+        return szerkesztve;
+    }
+
+    public void like(){
+        this.likeok++;
+    }
 }
